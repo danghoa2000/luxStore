f3e93b9a79efdecb5b63d6ca3e9a64df7d6e3a85
Hoa.nd-beetech <hoa.nd@beetechsoft.vn>
2023-02-01 15:00:51 +0700

    a

--- resources/js/index.jsx
+++ resources/js/index.jsx
@@ -4,10 +4,12 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('App');
+const container = document.getElementById("App");
 const root = createRoot(container);
 root.render(
-    <I18nextProvider i18n={i18n}>
-        <App />
-    </I18nextProvider>
+    <div>
+        <I18nextProvider i18n={i18n}>
+            <App />
+        </I18nextProvider>
+    </div>
 );

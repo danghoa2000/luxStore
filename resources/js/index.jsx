ff9236a55753ea51ffcea89c835704cc39d4c5ce
Hoa.nd-beetech <hoa.nd@beetechsoft.vn>
2023-02-01 10:45:10 +0700

    a

--- resources/js/index.jsx
+++ resources/js/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import App from "./app";
+import App from "./App";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import { createRoot } from "react-dom/client";

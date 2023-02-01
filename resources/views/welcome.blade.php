f4270991f748c8593c320873a4c9a6645c1468af
Hoa.nd-beetech <hoa.nd@beetechsoft.vn>
2023-02-01 10:51:48 +0700

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <title>Laravel</title>
-        <base href="http://127.0.0.1:8000/"/>
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 

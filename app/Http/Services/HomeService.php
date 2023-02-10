71d06005a11dfe0e21f3aaa6f20d5d99e75a19b5
danghoa2000 <hoacon200@gmail.com>
2023-02-11 00:06:42 +0700

    a

--- app/Http/Services/HomeService.php
+++ app/Http/Services/HomeService.php
@@ -15,11 +15,7 @@ class HomeService
     public function index()
     {
         try {
-            set_time_limit(0);
-            ini_set('max_execution_time', 180);
-
-
-
+ 
             $flashDelas = Product::where('sale_type', '<>', -1)
             ->where('expried', '>=', Carbon::now())
             ->with('productDetail:qty,sold_qty,product_id,price')
@@ -85,8 +81,8 @@ class HomeService
         return response()->json([
             'flashDelas' => $flashDelas,
             'newArrivals' => $newArrivals,
-            'ortherProduct' => $ortherProduct,
-            'topRateProduct' => $topRateProduct,
+            // 'ortherProduct' => $ortherProduct,
+            // 'topRateProduct' => $topRateProduct,
             'brand' => $brand,
             'message' => 'success!',
             'code' => Response::HTTP_OK
